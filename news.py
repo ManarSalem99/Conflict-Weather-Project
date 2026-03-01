0a7a25869daa2d29715c31ccf87232538fda0a86
Student A <msalem22@posta.pau.edu.tr>
2026-03-01 22:27:14 +0300

    experiment to test sth

--- news.py
+++ news.py
@@ -1,6 +1,6 @@
 def get_info():
 
-    headline = "Git workshop completed very successfully"
+    headline = "Git workshop completed successfully"
     category = "Technology"
 
     return "News: " + headline + " (" + category + ")"
